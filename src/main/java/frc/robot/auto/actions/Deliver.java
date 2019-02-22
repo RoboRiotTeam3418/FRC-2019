de46cf74e3874d268f01aaedb273dfc186e80e6e
Masterknight55 <lmantheyplymouth@gmail.com>
2019-02-22 12:31:27 -0600

    deliver test

---
+++ src/main/java/frc/robot/auto/actions/Deliver.java
@@ -0,0 +1,26 @@
+package frc.robot.auto.actions;
+
+public class Deliver implements Action  
+{
+
+    public void start()
+    {
+        
+    }
+    
+    public void update()
+    {
+
+    }
+    
+    public boolean isFinished()
+    {
+        
+    }
+    
+    public void done()
+    {
+
+    }
+
+}
